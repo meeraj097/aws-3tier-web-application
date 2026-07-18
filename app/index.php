ab3be3161426528b1bd9c93c93b6d1e94dba0f0f
EC2 Default User <ec2-user@ip-10-0-1-133.us-west-1.compute.internal>
2026-07-18 12:26:33 +0000

    Add PHP application source code

--- app/index.php
+++ app/index.php
@@ -1,3 +1,78 @@
 <?php
-// Application source code will be added here.
+$host = "enterprise-mysql-db.cvimmyc4ym5w.us-west-1.rds.amazonaws.com";
+$user = "<DB-USERNAME>";
+$password = "<DB-Password>";
+$database = "employee_db";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM employees";
+$result = $conn->query($sql);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Database</title>
+    <style>
+        body {
+            font-family: Arial;
+            background: #f4f4f4;
+            margin: 40px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 70%;
+            background: white;
+        }
+
+        th, td {
+            border: 1px solid #ccc;
+            padding: 12px;
+            text-align: center;
+        }
+
+        th {
+            background: #007bff;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+<h1>Employees from Amazon RDS</h1>
+
+<table>
+
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Department</th>
+<th>Salary</th>
+</tr>
+
+<?php
+while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>".$row['id']."</td>";
+    echo "<td>".$row['name']."</td>";
+    echo "<td>".$row['department']."</td>";
+    echo "<td>".$row['salary']."</td>";
+    echo "</tr>";
+}
+?>
+
+</table>
+
+</body>
+</html>
